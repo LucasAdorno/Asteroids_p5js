105262bf4db6d8b99e5c5197711cfaa34de62fb4
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-25 04:19:44 -0300

    infinity screen add

--- sketch.js
+++ sketch.js
@@ -9,10 +9,14 @@ function draw() {
   background(0);
   ship.render();
   ship.turn(0);
+  ship.update();
+  ship.edges();
 }
 
 function keyReleased() {
   ship.setRotation(0);
+  ship.boosting(true);
+
 }
 
 function keyPressed(){
@@ -22,6 +26,9 @@ function keyPressed(){
   else if(keyCode == LEFT_ARROW){
     ship.setRotation(-0.1);
   }
+  else if(keyCode == UP_ARROW){
+    ship.boosting(true);
+  }
 }
 
 function Ship(){
@@ -29,12 +36,48 @@ function Ship(){
   this.r = 25;
   this.heading = 0;
   this.rotation = 0;
+  this.vel = createVector(0,0);
+  this.isBoosting = false;
+
+  this.boosting = (b) => {
+    this.isBoosting = b;
+  }
+  
+  this.update = () => {
+    if (this.isBoosting){
+      this.boost();
+    }
+    this.pos.add(this.vel);
+    this.vel.mult(0.99)
+  };
+
+  this.boost = () => {
+    let force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+  }
+
   this.render = () =>{
     translate(this.pos.x, this.pos.y);
-    rotate(this.heading);
+    rotate(this.heading + PI/2);
     noFill();
     stroke(255);
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  }
+
+  this.edges = () =>{
+    if(this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    }
+    else if(this.pos.x < -this.r){
+      this.pos.x = width + this.r;
+    }
+    else if(this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    }
+    else if(this.pos.y < -this.r){
+      this.pos.y = height + this.r;
+    }
   }
 
   this.setRotation = (a) =>{
